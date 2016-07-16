a9a568935cc552507084e42eaee5c188d972e468
Vincent Bevia <vbevia@gmail.com>
2016-07-16 14:15:57 +0200

    fixed SearchJSONRequest, add key and other modifiers

--- app/src/main/java/com/corebaseit/flickrcoolapp/restful/SearchJSONRequest.java
+++ app/src/main/java/com/corebaseit/flickrcoolapp/restful/SearchJSONRequest.java
@@ -6,6 +6,7 @@ import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Response.ErrorListener;
 import com.android.volley.Response.Listener;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.corebaseit.flickrcoolapp.FlickrAPI.Keys;
 
 import org.json.JSONObject;
 
@@ -14,9 +15,27 @@ import org.json.JSONObject;
  */
 
 public class SearchJSONRequest extends JsonObjectRequest {
-    private static final String URL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=370fcc38fea136afa89f2bfcf4ca52d5&format=json&nojsoncallback=1&tags=%s&extras=url_z&per_page=500,tags";
 
+    private static String URL = getURL();
     private static final int MY_SOCKET_TIMEOUT_MS = 30000;
+
+    public static String getURL() {
+        return buildFlickrUri();
+    }
+
+    private static String buildFlickrUri() {
+
+        StringBuffer sb = new StringBuffer("https://api.flickr.com/services/rest/?method=flickr.photos.search");
+        sb.append("&api_key=" + Keys.getApiKey());
+        sb.append("&format=" + "json&nojsoncallback=1");
+        sb.append("&tags=" + "%s");
+        sb.append("&extras=" + "url_z");
+        sb.append("&per_page=500,tags");
+
+        sb.toString();
+        return sb.toString();
+    }
+
 
     public SearchJSONRequest(String tags, Listener<JSONObject> listener, ErrorListener errorListener) {
         super(Method.GET, String.format(URL, tags), listener, errorListener);
